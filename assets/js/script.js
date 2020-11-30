772934bf2c60438a848f6dc6e486f6ef5c6217f3
chandru9192 <chandrasekar@sketchbrahma.com>
2020-11-30 15:43:45 +0530

    basic calc

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,38 +12,42 @@
 //   document.getElementById('input').innerHTML = x;
 // }
 
-// window.onload = function () {
-//   var elements = document.getElementsByClassName('value');
-//   var screen = document.querySelectorAll(' p')[0];
-//   var clear = document.getElementsByClassName('clear')[0];
+//
 
-//   for (var i = 0; i < elements.length; i += 1) {
-//     if (elements[i].innerHTML === '=') {
-//       elements[i].addEventListener('click', calculate(i));
-//     } else {
-//       elements[i].addEventListener('click', addtocurrentvalue(i));
-//     }
-//   }
+'use strict';
+window.onload = function () {
+  document
+    .getElementById('enter_button')
+    .addEventListener('click', calculateAnswer);
+  let firstNumber = 0;
+  let operatorDropDown = '';
+  let secondNumber = 0;
+  let finalAnswer = 0;
 
-//   function addtocurrentvalue(i) {
-//     return function () {
-//       if (elements[i].innerHTML === '÷') {
-//         screen.innerHTML += '/ ';
-//       } else if (elements[i].innerHTML === 'x') {
-//         screen.innerHTML += '*';
-//       } else {
-//         screen.innerHTML += elements[i].innerHTML;
-//       }
-//     };
-//   }
+  function calculateAnswer() {
+    firstNumber = Number(document.getElementById('first_number').value);
+    secondNumber = Number(document.getElementById('second_number').value);
+    operatorDropDown = document.getElementById('operator').value;
+    if (!isNaN(!firstNumber) && !isNaN(!secondNumber)) {
+      switch (operatorDropDown) {
+        case '+':
+          finalAnswer = firstNumber + secondNumber;
+          break;
 
-//   clear.onclick = function () {
-//     screen.innerHTML = '';
-//   };
+        case '×':
+          finalAnswer = firstNumber * secondNumber;
+          break;
 
-//   function calculate(i) {
-//     return function () {
-//       screen.innerHTML = eval(screen.innerHTML);
-//     };
-//   }
-// };
+        case '-':
+          finalAnswer = firstNumber - secondNumber;
+          break;
+
+        case '÷':
+          finalAnswer = firstNumber / secondNumber;
+          break;
+      }
+      document.getElementById('answer').innerHTML = finalAnswer;
+    } else {
+    }
+  }
+};
